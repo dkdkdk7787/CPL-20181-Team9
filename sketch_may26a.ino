e2e8f93423c3111ec52b19cc4664310de04d1c39
LeeGyuDong <dlrbehd06@naver.com>
2018-05-28 18:30:29 +0900

    2018 05 28

--- sketch_may26a.ino
+++ sketch_may26a.ino
@@ -9,7 +9,14 @@ void loop();
 uint8_t touch_flag = 1;
 uint8_t press_flag = 0;
 uint8_t mp3_flag = 1;
+
 uint8_t current_mp3 = -1;
+
+int r0 = 0;
+int r1 = 0;
+int r2 = 0;
+
+int vias = 1;
 
 Adafruit_MPR121 cap = Adafruit_MPR121();
 uint16_t lasttouched = 0;
@@ -53,6 +60,33 @@ void setup() {
 
 void loop() {
 
+   if(press_flag > 0)
+   {
+      r0 = analogRead(A0);
+      r1 = analogRead(A1);
+      r2 = analogRead(A2);
+      
+      //if(r0 != 0)
+      {
+        //Serial.print("a0 :");
+       //Serial.print(r0);
+        //Serial.print("\n"); 
+      }
+  
+      //if(r1 != 0)
+      {
+       // Serial.print("a1 :");
+        //Serial.print(r1);
+        //Serial.print("\n");
+      }
+       //if(r2 != 0)
+      {
+        //Serial.print("a2 :");
+        //Serial.print(r2);
+        //Serial.print("\n");
+      }
+   }
+
   
    if(touch_flag > 0)
    {
@@ -69,8 +103,43 @@ void loop() {
             {
               Serial.print("play\n");
               current_mp3 = i;
-              mp3_play (current_mp3 / 2 * 3 + 1);    //0002 파일 플레이 , next
-              //mp3_play (1);    //0002 파일 플레이 , next
+              
+              if(press_flag > 0)
+              {
+                
+                if( i <= 2) //r0 센서
+                {
+                  if(r0 <= 10)
+                    vias = 1;
+                  else if( r0 <= 50 )
+                    vias = 3;
+                  else
+                    vias = 6;
+                }
+                else if( i <= 6) //r1 센서
+                {
+                  if(r1 <= 20)
+                    vias = 1;
+                  else if( r1 <= 23 )
+                    vias = 3;
+                  else
+                    vias = 6;
+                }
+                else if( i <= 10) //r2 센서
+                {
+                  if(r2 <= 0)
+                    vias = 1;
+                  else if( r2 <= 2 )
+                    vias = 3;
+                  else
+                    vias = 6;
+                }   
+                    
+              }
+             
+
+              mp3_play (17 - (current_mp3 / 2 * 3 + vias) ); 
+              
             }
             
           }
@@ -86,27 +155,6 @@ void loop() {
    }
 
     
- 
-  
-  /*
-  int r0 = analogRead(A0);
- // int r1 = analogRead(s1);
-  if(1)
-  {
-    Serial.print("a0 :");
-    Serial.print(r0);
-    Serial.print("\n");
-  }
-  /*
-  //if(r1 != 0)
-  //{
-   // Serial.print("a1 :");
-    //Serial.print(r1);
-  //  Serial.print("\n");
- // }
-  delay(100);
-    */
-
 
    
 }
